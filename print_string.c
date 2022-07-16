09a0806d740327fa38d98b9bed457f1852f03a12
Heiswill <willtriger@gmail.com>
2022-07-16 10:00:13 -0700

    updateon task5

---
+++ print_string.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: cahr tp print
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+
+/**
+ * print_S - prints a string and nonprintable char ascii values
+ * @arg: string to format
+ * Return: number of char printed
+ */
+int print_S(va_list args)
+{
+	unsigned int i;
+	int count = 0;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count += hex_print(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
